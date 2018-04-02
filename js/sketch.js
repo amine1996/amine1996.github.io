c7aed5239861bcea258881fba36fa813a18f271c
amine1996 <amine_ismail@hotmail.fr>
2018-04-02 18:05:27 +0200

    Update

--- js/sketch.js
+++ js/sketch.js
@@ -2,18 +2,19 @@ function setup()
 {
   createCanvas(screen.width, screen.height);
   background(255,255,255);   
+  angleMode(DEGREES);
 }
 
 function draw() 
 {
   background(255,255,255);
-
+  
   textSize(20);
 
   if(rotationX != null && rotationY != null && rotationZ != null)
   {
     fill(255,0 ,255);
-    ellipse(screen.width*map(cos(radians(rotationX)),-1,1,0,1), screen.height/2, 80, 80);
+    ellipse(screen.width*map(cos(rotationX),-1,1,0,1), screen.height/2, 80, 80);
     /*
 
     fill(128,0,0);
