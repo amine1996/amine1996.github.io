72a6d6aa4bd59e826de517518143800e11367e42
amine1996 <amine_ismail@hotmail.fr>
2018-04-03 21:18:49 +0200

    Update

--- js/sketch.js
+++ js/sketch.js
@@ -20,7 +20,7 @@ function draw()
   if(rotationX != null && rotationY != null && rotationZ != null)
   {
     fill(0,255,0);
-    ellipse(screen.width*map(cos(rotationY),-1,1,0,1), screen.height/2, 80, 80);
+    ellipse(screen.width/2*, screen.height*map(cos(rotationY),-1,1,0,1), 80, 80);
   }
   else
   {
