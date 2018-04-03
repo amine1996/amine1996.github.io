2641961980deea24411c4262f847df650e143ca8
amine1996 <amine_ismail@hotmail.fr>
2018-04-03 21:36:15 +0200

    Update

--- js/sketch.js
+++ js/sketch.js
@@ -21,6 +21,9 @@ function draw()
   {
     fill(0,255,0);
     ellipse(screen.width/2, screen.height*map(cos(rotationY),-1,1,0,1), 80, 80);
+
+    fill(255,0,0);
+    ellipse(screen.width*map(cos(rotationY+90),-1,1,0,1),screen.height/2, 80, 80);
   }
   else
   {
