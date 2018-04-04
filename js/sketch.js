62e298aa3b2a2e36292146f43668862e1c8dbbdc
amine1996 <amine_ismail@hotmail.fr>
2018-04-04 11:36:06 +0200

    Update

--- js/sketch.js
+++ js/sketch.js
@@ -36,11 +36,11 @@ function draw()
     text("RotationZ : "+rotationZ,50,100);
 
     //Bulle verticale
-    fill(0,0,255);
+    fill(255,0,0);
     ellipse(screen.width/2, screen.height*map(cos(lastRotationX),-1,1,0.05,0.95), 80, 80);
 
     //Bulle horizontale
-    fill(0,255,0);
+    fill(0,255,255);
     ellipse(screen.width*map(cos(lastRotationY+90),-1,1,0.05,0.95),screen.height/2, 80, 80);
 
     let deltaRotationY = rotationY - lastRotationY;
