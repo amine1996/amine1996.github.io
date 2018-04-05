8a04d7f79eaef538683112aa25d0fbe483e01ce1
amine1996 <amine_ismail@hotmail.fr>
2018-04-05 11:55:59 +0200

    Update

--- js/sketch.js
+++ js/sketch.js
@@ -47,7 +47,7 @@ function draw()
 
     fill(0,0,255);
     //ellipse(screen.width*map(cos(horizontalBubbleAngle+90),-1,1,0.05,0.95),screen.height/2, 80, 80);
-    ellipse(screen.width*b,screen.height/2, 80, 80);
+    ellipse(screen.width*(b+1/2),screen.height/2, 80, 80);
     //Vertical bubble
     //OK
     let deltaRotationX = sin(rotationX) - sin(verticalBubbleAngle);
