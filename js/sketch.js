6370d168043da27d4e2f4c6b245fdb55b0495bb2
amine1996 <amine_ismail@hotmail.fr>
2018-04-05 13:28:40 +0200

    Update

--- js/sketch.js
+++ js/sketch.js
@@ -33,55 +33,31 @@ function draw()
   //A plat
   if(rotationY != null && rotationX != null)
   {
+    rotationVector = getWGSRotationVector();
+
     //Horizontal bubble
-    //Not working properly
-
-    let deltaRotationY = cos(rotationY) - cos(horizontalBubbleAngle);
-    horizontalBubbleAngle += map(deltaRotationY,-2,2,-5,5)
-
-    let a = -cos(rotationZ)*sin(rotationY)-sin(rotationZ)*sin(rotationX)*cos(rotationY);
-    let b = -sin(rotationZ)*sin(rotationY)+cos(rotationZ)*sin(rotationX)*cos(rotationY);
-    let c = -cos(rotationX)*cos(rotationY);
-
-
-
     fill(0,0,255);
-    //ellipse(screen.width*map(cos(horizontalBubbleAngle+90),-1,1,0.05,0.95),screen.height/2, 80, 80);
-    ellipse(screen.width*(b+1/2),screen.height/2, 80, 80);
+    ellipse(screen.width*(rotationVector.y+1/2),screen.height/2, 80, 80);
+    
     //Vertical bubble
-    //OK
-    let deltaRotationX = sin(rotationX) - sin(verticalBubbleAngle);
-    verticalBubbleAngle += map(deltaRotationX,-2,2,-5,5)
-
     fill(255,0,0);
-    ellipse(screen.width/2, screen.height*map(cos(verticalBubbleAngle+90),-1,1,0.05,0.95), 80, 80);
-        
-    if(DEBUG)
-    {
-      fill(0,0,0);
-      text("RotationX : "+a,50,40);
-      text("RotationY : "+b,50,70);
-      text("RotationZ : "+c,50,100);
-
-      text("verticalBubbleAngle (RotationX) : "+verticalBubbleAngle,50,140);
-      text("horizontalBubbleAngle (RotationY) : "+horizontalBubbleAngle,50,170);
-    }
+    ellipse(screen.width/2, screen.height*(rotationVector.x+1/2), 80, 80);
   }
   else
   {
     text("No rotation found",10,30);
   }
 }
-//Not used for the moment-
-function resizeOnOrientationChange()
-{
-  if(lastOrientation != deviceOrientation)
-  {
-    if(deviceOrientation == LANDSCAPE)
-      resizeCanvas(screen.height,screen.width);
-    else
-      resizeCanvas(screen.width,screen.height);
 
-    lastOrientation = deviceOrientation;
-  }
+/*
+ * http://wiki.cs.vsb.cz/images/archive/6/69/20160405105728!TAMZ-new-L8.pdf
+ * Page 5
+ */
+function getWGSRotationVector()
+{
+  let Rx = -cos(rotationZ)*sin(rotationY)-sin(rotationZ)*sin(rotationX)*cos(rotationY);
+  let Ry = -sin(rotationZ)*sin(rotationY)+cos(rotationZ)*sin(rotationX)*cos(rotationY);
+  let Rz = -cos(rotationX)*cos(rotationY);
+
+  return createVector(Rx,Ry,Rz);
 }
